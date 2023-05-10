7087e38eee5be3c80e0c0962e5a3ce32db0559b9
RosarioLopezZapata <rosariolopezz.sm@gmail.com>
2023-05-10 11:09:06 +0200

    fourth commit

--- js/app.js
+++ js/app.js
@@ -9,7 +9,41 @@ const App = {
         squares: document.querySelectorAll('[data-id="square"]'),
     },
     state: {
-        currentPlayer: 1,
+        moves: [],
+    },
+    getGameStatus(moves) {
+
+        const p1Moves = moves.filter(move => move.playerId === 1).map(move => +move.squareId);
+        const p2Moves = moves.filter(move => move.playerId === 2).map(move => +move.squareId);
+
+
+        //Check if there's a winner or tie game
+
+        const winningPatterns = [
+            [1, 2, 3],
+            [1, 5, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 5, 7],
+            [3, 6, 9],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+        let winner = null;
+
+        winningPatterns.forEach(pattern => {
+            const p1Wins = pattern.every(v => p1Moves.includes(v));
+            const p2Wins = pattern.every(v => p2Moves.includes(v));
+
+            if (p1Wins) winner = 1;
+            if (p2Wins) winner = 2;
+        });
+
+        return {
+            status: moves.length === 9 || winner != null ? 'complete' : 'in-progress', // in progress | complete
+            winner,// 1 | 2 | null
+
+        }
     },
     init() {
         App.registerEventListeners();
@@ -27,15 +61,22 @@ const App = {
 
         this.$.squares.forEach(square => {
             square.addEventListener('click', event => {
+                //Check if there is already a play, if so return early
 
-
-                console.log(`Square with id ${event.target.id} was clicked`);
-                console.log(`Current player is ${this.state.currentPlayer}`);
-                if (square.hasChildNodes()) {
+                const hasMove = (squareId) => {
+                    const existingMove = App.state.moves.find(move => move.squareId === squareId)
+                    return existingMove !== undefined;
+                }
+                if (hasMove(+square.id)) {
                     return
                 }
+
+                //Determine which playericon to add to the square
                 const icon = document.createElement('i');
-                const currentPlayer = App.state.currentPlayer;
+
+                const lastMove = App.state.moves.at(-1);
+                const getOppositePlayer = (playerId) => playerId === 1 ? 2 : 1;
+                const currentPlayer = App.state.moves.length === 0 ? 1 : getOppositePlayer(lastMove.playerId);
                 if (currentPlayer === 1) {
 
                     icon.classList.add('fa-solid', 'fa-x', 'yellow');
@@ -44,8 +85,26 @@ const App = {
                     icon.classList.add('fa-solid', 'fa-o', 'turquoise');
                 }
 
-                App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+                App.state.moves.push({
+                    squareId: +square.id,
+                    playerId: currentPlayer,
+                });
+
+                console.log(App.state)
                 square.replaceChildren(icon);
+
+                // Check if there is a winner or a tie game
+                const game = App.getGameStatus(App.state.moves);
+
+
+                if (game.status === 'complete') {
+                    if (game.winner) {
+                        alert(`Player ${game.winner} wins!`);
+                    } else {
+                        alert('Tie!');
+                    }
+                }
+
             });
         });
 
